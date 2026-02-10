f9f2e39365965ee43cffdb933c2f3053cc928dd4
Saad123Khan <sk5908774@gmail.com>
2026-02-10 08:14:59 -1200

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -3,20 +3,18 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
+  base: "/", // 🔥 VERY IMPORTANT FOR VERCEL
+
   plugins: [
     react(),
-    mode === 'development' &&
-    componentTagger(),
+    mode === "development" && componentTagger(),
   ].filter(Boolean),
+
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
+
 }));
